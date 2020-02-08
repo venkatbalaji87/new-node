f667621ffa6794087c472b6c367c3a6d9a6277ab
venkatbalaji <venkatb2206@gmail.com>
2020-02-08 10:12:50 +0530

    added package.json

--- src/es6.js
+++ src/es6.js
@@ -3,3 +3,14 @@ const marks = [20,30,40,06,70];
 const increaseMarks = marks.map(mark, markIndex) => {
   console.log(mark, markIndex);
 });
+
+/**
+ * const increaseMarks = marks.map(mark) => {
+ *  return mark = 5;
+ * });
+ */
+
+/**
+ * Array mehtods are treated as expression
+ */
+console.log("Increased Mark", increasemark);
